6989883e8082f074a358dd481453185c3a9ba566
nicolle <tottol.16.06@gmail.com>
2026-03-30 11:09:24 -0500

    Primer comentario

---
+++ Contador.py
@@ -0,0 +1,8 @@
+palabra = input("Ingresa una palabra")
+palabra = palabra.lower()
+vocales = "aeiou"
+contador = 0
+for letra in palabra:
+    if letra in vocales:
+        contador += 1
+print("La palabra tiene", contador, "vocales")
